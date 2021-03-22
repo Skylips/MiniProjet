fe900d39e8bb90f7dd4f702f05f15058b3f0df9c
ANDRE Louis <louis.gabriel.andre@gmail.com>
2021-03-23 00:50:03 +0100

    Traduction_2

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -40,22 +40,12 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
+            $role = ["ROLE_USER"];
+            $user->setRoles($role);
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
-
-            /*
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('kingloulou44@gmail.com', 'AdminMail'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
-            // do anything else you need here, like send an email
-            */
 
             return $this->redirectToRoute('homepage');
         }
